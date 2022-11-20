38992aa76bc717dd0058abe24a87bb7d4d9715b6
raunak114 <thakurraunak114@gmail.com>
2022-11-21 02:20:22 +0530

    Initial Commit

--- core/src/com/tankstars/game/TankStars.java
+++ core/src/com/tankstars/game/TankStars.java
@@ -1,18 +1,32 @@
 package com.tankstars.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import java.util.ArrayList;
 
 public class TankStars extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	private final int MAIN_MENU = 0;
+	private int currStage;
+	private ArrayList<Stage> stages;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		stages = new ArrayList<>();
+		stages.add(new Stage(new ScreenViewport()));
+		currStage = MAIN_MENU;
+		Gdx.input.setInputProcessor(stages.get(currStage));
+
+
 	}
 
 	@Override
