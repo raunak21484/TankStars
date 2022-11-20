8db3ed2b16a339afcea4dbaf481294c0f7763e86
raunak114 <thakurraunak114@gmail.com>
2022-11-21 04:20:44 +0530

    Initial Commit

--- core/src/com/tankstars/game/TankStars.java
+++ core/src/com/tankstars/game/TankStars.java
@@ -2,9 +2,13 @@ package com.tankstars.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -12,34 +16,36 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 import java.util.ArrayList;
 
 public class TankStars extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	private final int MAIN_MENU = 0;
+
+	private final int MAIN_MENU = 1;
+	private final int LOADING_SCREEN = 0;
 	private int currStage;
 	private ArrayList<Stage> stages;
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
 		stages = new ArrayList<>();
 		stages.add(new Stage(new ScreenViewport()));
-		currStage = MAIN_MENU;
+		currStage = LOADING_SCREEN;
 		Gdx.input.setInputProcessor(stages.get(currStage));
-
+		Image img = new Image(new Sprite(new Texture(Gdx.files.internal("MainMenu/loadingscreen.png"))));
+		stages.get(currStage).addActor(img);
 
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		stages.get(currStage).act(Gdx.graphics.getDeltaTime());
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		Actor tImg = stages.get(currStage).getActors().first();
+		tImg.setColor(tImg.getColor().r,tImg.getColor().g,tImg.getColor().b,tImg.getColor().a-0.002f);
+		stages.get(currStage).draw();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		for(int i=0;i<stages.size();i++){
+			stages.get(i).dispose();
+		}
 	}
 }
--- desktop/src/com/tankstars/game/DesktopLauncher.java
+++ desktop/src/com/tankstars/game/DesktopLauncher.java
@@ -10,6 +10,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Tank Stars");
+		config.setWindowedMode(1920,887);
 		new Lwjgl3Application(new TankStars(), config);
 	}
 }
