bb0d105efdf7cdd3b1410f73d7b035fdc0c079e4
raunak114 <thakurraunak114@gmail.com>
2022-11-21 22:50:49 +0530

    Commit $number$

--- core/src/com/tankstars/game/Actors/Logo.java
+++ core/src/com/tankstars/game/Actors/Logo.java
@@ -1,16 +1,34 @@
 package com.tankstars.game.Actors;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 public class Logo extends Image {
+    Sound sound;
+    long id;
+    boolean isPressed = false;
     public Logo(int width, int height, int x, int y){
         super(new Texture(Gdx.files.internal("MainMenu/logo.png")));
         setBounds(x,y,width,height);
+        sound = Gdx.audio.newSound(Gdx.files.internal("tempsound1.mp3"));
 
+        addListener(new InputListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                if(!isPressed){
+                    isPressed = true;
+                    id = sound.play();
+
+                }
+                return true;
+            }
+        });
     }
 
     @Override
