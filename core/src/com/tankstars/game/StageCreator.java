084575851be048fba528845164285ecc98ecf236
raunak114 <thakurraunak114@gmail.com>
2022-11-22 00:49:51 +0530

    Commit $number$

--- core/src/com/tankstars/game/StageCreator.java
+++ core/src/com/tankstars/game/StageCreator.java
@@ -6,17 +6,18 @@ import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.tankstars.game.Actors.ButtonActor;
 
 public class StageCreator {
     public Stage initLoadingScreen(InputMultiplexer mux){
         Stage stage = new Stage(new ScreenViewport());
         mux.removeProcessor(mux.size()-1);
         mux.addProcessor(stage);
-        Gdx.input.setInputProcessor(stage);
         Image img = new Image(new Sprite(new Texture(Gdx.files.internal("MainMenu/loadingscreen.png"))));
         img.setSize(1920,887);
         stage.addActor(img);
@@ -26,7 +27,8 @@ public class StageCreator {
         mux.removeProcessor(mux.size()-1);
         Stage stage = new Stage(new ScreenViewport());
         mux.addProcessor(stage);
-        Image logo = new Image(new Sprite(new Texture(Gdx.files.internal("MainMenu/logo.png"))));
+        //Image logo = new Image(new Sprite(new Texture(Gdx.files.internal("MainMenu/logo.png"))));
+        ButtonActor logo = new ButtonActor("MainMenu/logo.png",150,100,100,50);
         //logo.setScaling(new Scaling() {
 
         Image background = new Image(new Sprite(new Texture(Gdx.files.internal("badlogic.jpg"))));
--- core/src/com/tankstars/game/TankStars.java
+++ core/src/com/tankstars/game/TankStars.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
@@ -16,6 +17,7 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import com.tankstars.game.Actors.ButtonActor;
 
 import java.util.ArrayList;
 
@@ -28,16 +30,18 @@ public class TankStars extends ApplicationAdapter implements InputProcessor {
 	private ArrayList<Stage> stages;
 	private Boolean bool;
 	private Actor tempActor;
+	private Vector2 coord;
 	InputMultiplexer mux;
 	@Override
 	public void create () {
-		mux	= new InputMultiplexer(mux);
+		mux	= new InputMultiplexer();
 		mux.addProcessor(this);
 		stageCreator = new StageCreator();
 		stages = new ArrayList<>();
 		stages.add(stageCreator.initLoadingScreen(mux));
 		currStage = LOADING_SCREEN;
 		bool = false;
+		Gdx.input.setInputProcessor(mux);
 		Timer.schedule(new Timer.Task(){
 			@Override
 			public void run(){
@@ -95,6 +99,11 @@ public class TankStars extends ApplicationAdapter implements InputProcessor {
 
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		coord = this.stages.get(currStage).screenToStageCoordinates(new Vector2((float) screenX, (float) screenY));
+		Actor hitActor = this.stages.get(currStage).hit(coord.x, coord.y, false);
+		if(hitActor instanceof ButtonActor){
+
+		}
 		return false;
 	}
 
